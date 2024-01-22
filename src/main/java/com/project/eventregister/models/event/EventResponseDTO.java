634e2c3127d004ebe1fcd98f241bafe7c7215926
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-01-21 22:15:26 -0300

    chore: add DTOs for responses of classes Event and Participant

--- src/main/java/com/project/eventregister/models/event/EventResponseDTO.java
+++ src/main/java/com/project/eventregister/models/event/EventResponseDTO.java
@@ -1,2 +1,66 @@
-package com.project.eventregister.models.event;public record EventResponseDTO() {
+package com.project.eventregister.models.event;
+
+import com.project.eventregister.models.participant.Participant;
+import com.project.eventregister.models.participant.ParticipantResponseDTO;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public class EventResponseDTO {
+  private UUID id;
+  private String name;
+  private String description;
+  private List<ParticipantResponseDTO> participants;
+  private LocalDate startDate;
+  private LocalDate endDate;
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public List<ParticipantResponseDTO> getParticipants() {
+    return participants;
+  }
+
+  public void setParticipants(List<ParticipantResponseDTO> participants) {
+    this.participants = participants;
+  }
+
+  public LocalDate getStartDate() {
+    return startDate;
+  }
+
+  public void setStartDate(LocalDate startDate) {
+    this.startDate = startDate;
+  }
+
+  public LocalDate getEndDate() {
+    return endDate;
+  }
+
+  public void setEndDate(LocalDate endDate) {
+    this.endDate = endDate;
+  }
 }
--- src/main/java/com/project/eventregister/models/participant/ParticipantResponseDTO.java
+++ src/main/java/com/project/eventregister/models/participant/ParticipantResponseDTO.java
@@ -1,2 +1,42 @@
-package com.project.eventregister.models.participant;public class ParticipantResponseDTO {
+package com.project.eventregister.models.participant;
+
+import java.util.UUID;
+
+public class ParticipantResponseDTO {
+  private UUID id;
+  private String firstName;
+  private String lastName;
+  private String email;
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
 }
