4936e25aeb270ba9fe000cd41104813b8449cc1b
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-01-24 05:02:43 -0300

    chore: add date range DTO for requests in event controller

--- src/main/java/com/project/eventregister/models/event/dateRangeRequestDTO.java
+++ src/main/java/com/project/eventregister/models/event/dateRangeRequestDTO.java
@@ -1,2 +1,6 @@
-package com.project.eventregister.models.event;public record dateRangeRequestDTO() {
+package com.project.eventregister.models.event;
+
+import java.time.LocalDate;
+
+public record dateRangeRequestDTO(LocalDate startDate, LocalDate endDate) {
 }
