740d2d67d87bf2cd94ddd66867ba32860f81ab46
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-01-24 04:55:50 -0300

    feat: add a method that searches for events between specified dates

--- src/main/java/com/project/eventregister/repositories/EventRepository.java
+++ src/main/java/com/project/eventregister/repositories/EventRepository.java
@@ -2,6 +2,8 @@ package com.project.eventregister.repositories;
 
 import com.project.eventregister.models.event.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -10,5 +12,7 @@ import java.util.UUID;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, UUID> {
-  List<Event> findByStartDateBetween(LocalDate startDate, LocalDate endDate);
+  @Query("SELECT e FROM Event e WHERE e.startDate >= :startDate AND e.endDate <= :endDate")
+  List<Event> findEventsBetweenDates(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 }
+
