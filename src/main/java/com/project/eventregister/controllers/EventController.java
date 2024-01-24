a80798fd6bc06cdd3555d9d684f8de4f18d1e731
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-01-24 05:04:16 -0300

    feat: create the end-point of method that searches for events between specified dates

--- src/main/java/com/project/eventregister/controllers/EventController.java
+++ src/main/java/com/project/eventregister/controllers/EventController.java
@@ -3,11 +3,13 @@ package com.project.eventregister.controllers;
 import com.project.eventregister.models.event.EventDTO;
 import com.project.eventregister.models.event.Event;
 import com.project.eventregister.models.event.EventResponseDTO;
+import com.project.eventregister.models.event.dateRangeRequestDTO;
 import com.project.eventregister.services.EventService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -21,26 +23,28 @@ public class EventController {
   }
 
   @PostMapping("/create")
-  ResponseEntity<Event> registerANewEvent(@RequestBody EventDTO request) {
-    var event = service.registerANewEvent(request);
-    return ResponseEntity.status(HttpStatus.CREATED).body(event);
+  ResponseEntity<Event> registerANewEvent(@RequestBody EventDTO requestEventCreationData) {
+    return new ResponseEntity<>(service.registerANewEvent(requestEventCreationData), HttpStatus.CREATED);
   }
 
   @DeleteMapping("/remove/{id}")
-  ResponseEntity<HttpStatus> unregisterAEvent(@PathVariable(name = "id") UUID eventId) {
+  ResponseEntity<String> unregisterAEvent(@PathVariable(name = "id") UUID eventId) {
     service.unregisterAEvent(eventId);
-    return ResponseEntity.status(HttpStatus.OK).build();
+    return ResponseEntity.ok("Deleted event with successfully.");
   }
 
   @GetMapping("/get-all")
   ResponseEntity<List<EventResponseDTO>> getAllEvents() {
-    var events = service.getAllEvents();
-    return ResponseEntity.status(HttpStatus.OK).body(events);
+    return new ResponseEntity<>(service.getAllEvents(), HttpStatus.OK);
   }
 
   @GetMapping("/get-one/{id}")
   ResponseEntity<EventResponseDTO> getJustOneEvent(@PathVariable(name = "id") UUID eventId) {
-    var event = service.getJustOneEvent(eventId);
-    return ResponseEntity.status(HttpStatus.OK).body(event);
+    return new ResponseEntity<>(service.getJustOneEvent(eventId), HttpStatus.OK);
+  }
+
+  @GetMapping("/get-events-between-dates")
+  ResponseEntity<List<EventResponseDTO>> getEventsBetweenDates(@RequestBody dateRangeRequestDTO dataRangeRequest) {
+    return new ResponseEntity<>(service.findEventsBetweenDates(dataRangeRequest.startDate(), dataRangeRequest.endDate()), HttpStatus.OK);
   }
 }
